eb5c90aa4fccb5e619d88f272e32df8a2e5cefc3
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-04 15:48:11 -0700

    added functionality to determine the outcome

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ShapesMenu from './components/ShapesMenu';
 import { Container, Header } from 'semantic-ui-react';
 
 class App extends React.Component {
-  state = {userSelection: "Rock", computerSelection: "Rock"};
+  state = {userSelection: "Rock", computerSelection: "Rock", };
 
   getRandomSelection = () => {
     const selections = ["Rock", "Paper", "Scissors"];
@@ -13,8 +13,40 @@ class App extends React.Component {
   getUserSelection = (selection) => {
     this.setState({ 
       userSelection: selection,
-      computerSelection: this.getRandomSelection()
+      computerSelection: this.getRandomSelection(),
      });
+  };
+
+  getOutcome = () => {
+    switch(this.state.userSelection) {
+      case "Rock":
+        switch(this.state.computerSelection) {
+          case "Rock":
+            return "Tie";
+          case "Paper":
+            return "Loss";
+          case "Scissors":
+            return "Win";  
+        }
+      case "Paper":
+        switch(this.state.computerSelection) {
+          case "Rock":
+            return "Win";
+          case "Paper":
+            return "Tie";
+          case "Scissors":
+            return "Loss";  
+        }
+      case "Scissors":
+        switch(this.state.computerSelection) {
+          case "Rock":
+            return "Loss";
+          case "Paper":
+            return "Win";
+          case "Scissors":
+            return "Tie";  
+        }
+    }
   };
 
   render() {
@@ -28,6 +60,9 @@ class App extends React.Component {
         </div>
         <div>
           Computer selects: {this.state.computerSelection}
+        </div>
+        <div>
+          The outcome is:  {this.getOutcome()}
         </div>
       </Container>
     );
